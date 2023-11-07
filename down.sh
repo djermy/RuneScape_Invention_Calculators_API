085f2f1f467ef3a25b892e94d66ab4bc7eed3196
djermy <danjermy@protonmail.com>
2023-11-07 14:28:40 +0100

    updated docker compose file to use production image, removed nginx and all associated component files

--- down.sh
+++ down.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 USER_ID=$(id -u) GROUP_ID=$(id -g) docker-compose down
-docker rm rs-calc-api-app-server
-docker rm rs-calc-api-proxy-server
+docker rm rs-calc-api
 docker rm rs-calc-front-end
